7397987b4623cf7ab43a3e7a0bb6e98256e53aaa
Oleh <oleh.kepsha@mycit.ie>
2021-01-06 15:05:56 +0000

    Update menu for neon circle and replace images on battle

--- src/Page/EffectPage/Battle.tsx
+++ src/Page/EffectPage/Battle.tsx
@@ -13,22 +13,15 @@ export default ({battleGround}: BattleInput) => {
     let squad1: Array<Ally> = battleGround.allySquad.squad;
     let squad2: Array<Enemy> = battleGround.enemySquad.squad;
 
-    for(let i = 0; i < squad1.length; ++i){
-        let allyPerson: Ally = squad1[i];
-        let allyImageElement: JSX.Element = <img src={allyPerson.image} alt="Some Ally Hero" height="100px" width= "100px"/>
-        toShow.push(allyImageElement);
-    }
-
-    for(let i = 0; i < squad2.length; ++i){
-        let enemyPerson: Enemy = squad2[i];
-        let enemyImageElement: JSX.Element = <img src={enemyPerson.image} alt="Some Enemy Hero" height="100px" width= "100px"/>
-        toShow.push(enemyImageElement);
-    }
-
     return(
-        <div className="">
-            {battleGround.allySquad.squad.map(ally => <img src={ally.image} alt="Some Ally Hero" height="100px" width= "100px"/>)}
-            {toShow}
+        <div className="BattleGround">
+            <div className="BattleAlly">
+                {battleGround.allySquad.squad.map(ally => <img src={ally.image} alt="Some Ally Hero" height="300px" width= "300px"/>)}
+            </div>
+            <div className="BattleEnemy">
+                {battleGround.enemySquad.squad.map(Enemy => <img src={Enemy.image} alt="Some Enemy Here" height="300px" width= "300px"/>)}
+            </div>
+            
         </div>
     );
 }
